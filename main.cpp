2f490fc14e6afe7ee9f7f71824cf0f40f1df7b5b
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 16:40:36 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    double Number;
 
+    cout << "¬вед≥ть дробове число:  ";
+    cin >> Number;
+
+    int wholePart = Number;
+    double fractionalPart = Number - wholePart;
+    int kopecks = fractionalPart * 100;
+
+    if (kopecks == 0) {
+        cout << wholePart << " грн." << endl;
+    }
+    else {
+        cout << wholePart << " грн. " << kopecks << " коп." << endl;
+    }
     system("pause");
     return 0;
 }
