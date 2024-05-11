48dbde99b01cfe206bfc69dc649deb242183d6af
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 16:07:37 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
-
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double  dov;
+    double  shur;
+    double  vusota;
+    cout << "Обчислення об'єму пралелепіпеда" << endl;
+    cout << "Довжина (см) -> ";
+    cin >> dov;
+    cout << "Ширина (см) -> ";
+    cin >> shur;
+    cout << "Висота (см) -> ";
+    cin >> vusota;
+    double  plocha = dov * shur;
+    double  obyem = plocha * vusota;
+    cout << "Об'єм: " << obyem << "куб.см" << endl; 
     system("pause");
     return 0;
 }
