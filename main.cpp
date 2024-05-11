fab27c6e51f45d6772d7bbc371479e2ae63b5d2c
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 16:11:23 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <Windows.h>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double radius;
+    cout << "¬вед≥ть рад≥ус кул≥: ";
+    cin >> radius;
+    double volume = (4.0 / 3.0) * 3.14 * (radius * radius * radius);
+    cout << "ќб'Їм кул≥: " << volume << " куб≥чних одиниць.\n"; 
     system("pause");
     return 0;
 }
