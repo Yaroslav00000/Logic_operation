35917871517db8d9e54204ee5d44e148a95de898
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 16:20:24 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    int second, hours, min, seconds;
 
+    cout << "Введіть кількість секунд: ";
+    cin >> second;
+
+    hours = second / 3600;
+    second %= 3600;
+    min = second / 60;
+    seconds = second % 60;
+
+    cout << hours << " Год " << min << " Хв " << seconds << " Сек " <<  endl;
+
+    return 0;
     system("pause");
     return 0;
 }
