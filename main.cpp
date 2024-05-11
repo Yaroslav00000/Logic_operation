177daa0cada56fde87ed8a853b32d48c1fabf7d9
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 20:42:46 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,25 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    double distancemeters, distancekilometers,timeseconds, finaltimeseconds, speedkm_Hour;
+    int timeminutes;
 
+    cout << "Обчислення швидкості бігу.\n";
+    cout << "Введіть довжину дистанції (метрів) = ";
+    cin >> distancemeters;
+
+    cout << "Введіть час (хв) = ";
+    cin >> timeminutes;
+    cout << "І час (сек) = ";
+    cin >> timeseconds;
+
+    finaltimeseconds = (timeminutes * 60) + timeseconds;
+    distancekilometers = distancemeters / 1000;
+    speedkm_Hour = distancekilometers / (finaltimeseconds / 3600);
+
+    cout << "Дистанція: " << distancemeters << " м." << endl;
+    cout << "Час: " << timeminutes << " мин " << timeseconds << " сек = " << finaltimeseconds << " сек." << endl;
+    cout << "Ви бігли зі швидкістю " << speedkm_Hour << " км/год." << endl;
     system("pause");
     return 0;
 }
