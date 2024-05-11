a438305e1d30c257e5db3bbe9e0768d6835064ef
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 16:06:16 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int hryvna, kop;
 
+    cout << "¬вед≥ть суму в гривн€х: ";
+    cin >> hryvna;
+
+    cout << "¬вед≥ть суму в коп≥йках: ";
+    cin >> kop;
+
+    if (kop >= 100) {
+        hryvna += kop / 100;
+        kop %= 100;
+    }
+
+    cout << "—коригована сума: " << hryvna << " грн. " << kop << " коп." << endl;
     system("pause");
     return 0;
 }
