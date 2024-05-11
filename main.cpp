2b1411517abbe5b2e498adfd75c3e2c6115442bf
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 16:10:03 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
-
+    double mashtab, dis_cm;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Обчислення відстані між населеними пунктами.\n";
+    cout << "Масштаб карти (кількість кілометрів в одному сантиметрі) -> ";
+    cin >> mashtab;
+    cout << "Відстань між точками, що зображують населені пункти (см) -> ";
+    cin >> dis_cm;
+    double dis_km = mashtab * dis_cm;
+    cout << "Відстань між населеними пунктами " << dis_km << " км.\n";
     system("pause");
     return 0;
 }
