54c8b582e4465b962a8622bf667dc1cc40798585
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 21:37:59 +0300

    Task  complited

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int days,weeks,day;
+	cout << "¬вед≥ть к≥льк≥сть дн≥в: ";
+	cin >> days;
+	weeks = days / 7;
+	day = days % 7;
 
-    system("pause");
-    return 0;
+	cout << weeks << " тиждн≥ ≥ " << day << " дн≥." << endl;
+
+	return 0;
 }
